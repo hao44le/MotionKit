424295ce3fa98163a5fcea892fb66b07409704da
Grey <chen1474@purdue.edu>
2016-09-06 23:20:25 -0400

    first commit

---
+++ Example/Example/ViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  Example
+//
+//  Created by Haroon on 19/02/2015.
+//  Copyright (c) 2015 MotionKit. All rights reserved.
+//
+
+import UIKit
+import MotionKit
+
+class ViewController: UIViewController {
+
+    let motionKit = MotionKit()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        motionKit.getAccelerometerValues(1.0) { (x, y, z) -> () in
+            print(x)
+        }
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
