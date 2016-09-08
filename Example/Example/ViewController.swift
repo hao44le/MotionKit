7271f11b101006ca5abade65977945d72a79a4aa
Grey <chen1474@purdue.edu>
2016-09-07 20:31:15 -0400

    update

--- Example/Example/ViewController.swift
+++ Example/Example/ViewController.swift
@@ -15,9 +15,34 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        motionKit.getAccelerometerValues(1.0) { (x, y, z) -> () in
-            print(x)
+        
+        //want
+        motionKit.getAccelerometerValues(1/200) { (x, y, z) -> () in
+            print("\n getAccelerometerValues\n x:\(x) ,y:\(y) ,z:\(z)")
         }
+        
+        //want
+//        motionKit.getGyroValues(1.0) { (x, y, z) in
+//            print("\n getGyroValues\n x:\(x) ,y:\(y) ,z:\(z)")
+//        }
+        
+        //want
+//        motionKit.getMagnetometerValues(1.0) { (x, y, z) in
+//            print("\n getMagnetometerValues\n x:\(x) ,y:\(y) ,z:\(z)")
+//        }
+        
+        
+//        motionKit.getAttitudeAtCurrentInstant { (attitude) in
+//            print("\n getAttitudeAtCurrentInstant\n attitude:\(attitude)")
+//        }
+        
+//        motionKit.getGravityAccelerationFromDeviceMotion(1.0) { (x, y, z) in
+//            print("\n getGravityAccelerationFromDeviceMotion\n x:\(x) ,y:\(y) ,z:\(z)")
+//        }
+        
+//        motionKit.getRotationRateFromDeviceMotion(1.0) { (x, y, z) in
+//            print("\n getRotationRateFromDeviceMotion\n x:\(x) ,y:\(y) ,z:\(z)")
+//        }
         // Do any additional setup after loading the view, typically from a nib.
     }
 
